e167ce3265a763b355ba232edeb00b0175bd036c
Apipilikas <apipilikas@gmail.com>
2023-10-15 23:20:43 +0300

    Added new structure

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.use(cors());
 app.use(express.json());
+
+const mongodb = require('./mongodb');
+
+const client = mongodb.client;
+
+client.connect()
+.then(() => {
+    console.log("Database connected successfully!")
+    server.listen(8080);
+    console.log("Server is listening...");
+})
--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,10 @@
 const mongoClient = require('mongodb').MongoClient;
 
-const url = "mongodb+srv://Apipilikas:p3180157agg15@cluster.r0jin.mongodb.net/euvision-db?retryWrites=true&w=majority";
+const url = "mongodb+srv://Apipilikas:p3180157agg15@cluster.r0jin.mongodb.net/ev-db?retryWrites=true&w=majority";
 
 var client = new mongoClient(url);
 
-var eurovisionDB = client.db("euvision-db");
+var eurovisionDB = client.db("ev-db");
 var countries = eurovisionDB.collection("countries");
 var judges = eurovisionDB.collection("judges");
 
