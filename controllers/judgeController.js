d2de1e49dca295d6d356ebc13e5d3e8533ab63fb
Apipilikas <apipilikas@gmail.com>
2026-01-31 17:30:43 +0100

    Debugging checking on deployment 3

--- controllers/judgeController.js
+++ controllers/judgeController.js
@@ -63,6 +63,7 @@ module.exports.registerJudge = (req, res, next) => {
         record.addProperty(activationToken_ID, activationToken);
 
         record.saveAndApplyChanges().then(response => {
+            console.log(JSON.stringify(response));
             if (response.success) {
                 EmailProvider.sendActivateJudgeEmail(email, name, code, activationToken).then(response => {
                     if (response.success) {
